28747ad7bda6b3ff6749a3d3eb0af3b5c7903859
JulianCambraia <juliancambraia@gmail.com>
2021-06-08 23:34:11 -0300

    Dockerizing Microservices GitHub Action Docker Hub

---
+++ src/main/java/br/com/juliancambraia/dockerinzing/DockerinzingApplication.java
@@ -0,0 +1,13 @@
+package br.com.juliancambraia.dockerinzing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerinzingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerinzingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/juliancambraia/dockerinzing/DockerinzingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.juliancambraia.dockerinzing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerinzingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
